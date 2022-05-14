6f2769b37910dc3843f02d5be844bf0d641cf63d
AmirHossein <amirhossein.n2003@gmail.com>
2022-05-14 05:27:05 +0430

    Completing ProfileMenuTest

--- civilization/src/test/java/ProfileMenuTest.java
+++ civilization/src/test/java/ProfileMenuTest.java
@@ -18,31 +18,28 @@ import static org.mockito.Mockito.when;
 public class ProfileMenuTest {
     private final PrintStream standard = System.out;
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final Processor processor = Mockito.mock(Processor.class);
     private final ProfileMenu profileMenu = new ProfileMenu();
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         System.setOut(new PrintStream(outputStream));
+        User user = new User("abc", "ghi4JKL", "def");
+        UserController.getInstance().getUsers().add(user);
+        UserController.getInstance().setLoggedInUser(user);
     }
 
     @AfterEach
-    public void tearDown(){
+    public void tearDown() {
         System.setOut(standard);
         UserController.getInstance().setUsers(new ArrayList<>());
         UserController.getInstance().setLoggedInUser(null);
     }
 
-
     @Test
     public void checkChangeNickname() throws Exception {
-        Processor processor = Mockito.mock(Processor.class);
-
         when(processor.get("nickname")).thenReturn("defg");
         when(processor.getNumberOfFields()).thenReturn(1);
-
-        User user = new User("abc", "ghi4JKL", "def");
-        UserController.getInstance().getUsers().add(user);
-        UserController.getInstance().setLoggedInUser(user);
 
         Whitebox.invokeMethod(profileMenu, "changeNickname", processor);
 
@@ -52,7 +49,15 @@ public class ProfileMenuTest {
 
 
     @Test
-    public void checkChangePassword(){
-        
+    public void checkChangePassword() throws Exception {
+        when(processor.get("current")).thenReturn("ghi4JKL");
+        when(processor.get("new")).thenReturn("GHI4jkl");
+        when(processor.get("password")).thenReturn(null);
+        when(processor.getNumberOfFields()).thenReturn(3);
+
+        Whitebox.invokeMethod(profileMenu, "changePassword", processor);
+
+        Assertions.assertEquals(processor.get("new"), UserController.getInstance().getLoggedInUser().getPassword());
+        Assertions.assertEquals(outputStream.toString().trim(), "Password changed successfully!");
     }
 }
