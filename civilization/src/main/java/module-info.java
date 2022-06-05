1f00de225521b486bd431f6800c9164a2232923e
AmirHossein <amirhossein.n2003@gmail.com>
2022-06-05 05:21:53 +0430

    Adding gson to module-info

--- civilization/src/main/java/module-info.java
+++ civilization/src/main/java/module-info.java
@@ -3,9 +3,9 @@ module civilization {
     requires javafx.fxml;
     requires com.google.gson;
 
-    opens app.controllers to javafx.fxml;
+    opens app.controllers to javafx.fxml, com.google.gson;
     exports app.controllers;
-    opens app.models to javafx.fxml;
+    opens app.models to javafx.fxml, com.google.gson;
     exports app.models;
     opens app.views to javafx.fxml;
     exports app.views;
