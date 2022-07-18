b1d0705160995bf88554ea601b8859086686b696
SeyyedMohammadHossein Hosseini <s.m.m.m.h2013@gmail.com>
2022-07-18 22:36:11 +0430

    some progress

--- civilization/src/main/java/app/views/GameViewController.java
+++ civilization/src/main/java/app/views/GameViewController.java
@@ -4,6 +4,8 @@ import app.controllers.CivilizationController;
 import app.controllers.GameController;
 import app.models.Civilization;
 import app.models.map.CivilizationMap;
+import app.models.tile.Tile;
+import app.models.tile.VisibleTile;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
@@ -13,6 +15,9 @@ public class GameViewController {
     private final int VIEW_MAP_WIDTH = 9;
     private final int TILE_HEIGHT = 120;
     private final int TILE_WIDTH = 140;
+
+    private int mapX = VIEW_MAP_HEIGHT / 2;
+    private int mapY = VIEW_MAP_WIDTH / 2;
 
     @FXML
     private BorderPane pane;
@@ -36,6 +41,15 @@ public class GameViewController {
         Civilization civilization = GameController.getInstance().getCivilization();
         CivilizationMap personalMap = civilization.getPersonalMap();
 
-
-    }
+        for (int i = -(VIEW_MAP_HEIGHT / 2); i <= VIEW_MAP_HEIGHT / 2; i++) {
+            for (int j = -(VIEW_MAP_WIDTH / 2); j <= VIEW_MAP_WIDTH / 2; j++) {
+                if (j % 2 == 1 || j % 2 == -1) {
+                    if (mapY % 2 == 1 && i == -(VIEW_MAP_HEIGHT / 2)) continue;
+                    else if (mapY % 2 == 0 && i == VIEW_MAP_HEIGHT / 2) continue;
+                }
+                int x = mapX + i;
+                int y = mapY + j;
+                VisibleTile visibleTile = personalMap.getTileByXY(x, y);
+            }
+        }
 }
